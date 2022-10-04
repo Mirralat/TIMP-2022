2a66ba3e536857d72ce1d15e2a8aaf1df7278125
mirralat <67077296+mirralat@users.noreply.github.com>
2022-10-04 15:20:30 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,15 +3,5 @@
 python3 var.py
 python3 secure.py
 python3 nocreate.py
-sudo touch /etc/systemd/system/lock.service
-echo "[Unit]
-Description=My bot
-After=multi-user.target
- 
-[Service]
-Type=idle
-ExecStart=/usr/bin/python nocreate.py
-Restart=always
- 
-[Install]
-WantedBy=multi-user.target" >> lock.service
+sudo systemctl daemon-reload
+sudo systemctl start lock.service
