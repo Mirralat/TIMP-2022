59cd9e5dce818c7a75de15832f5d3540e5f5726c
mirralat <67077296+mirralat@users.noreply.github.com>
2022-11-17 05:58:12 -0500

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,10 +1,14 @@
 import os
+import os.path
 
 
 def start_the_installation(file, filename, data):
     need_path = f'{file}/{filename}'
-    need_data = open(need_path, 'w')
-    need_data.write(data)
+    if os.path.isfile(need_path):
+    	pass
+    else:
+	    need_data = open(need_path, 'w')
+	    need_data.write(data)
 
 
 print("Start the installation!")
@@ -25,6 +29,10 @@ try:
 except:
     FileExistsError
 
+with open("source.txt") as f:
+	data = f.read()
+
+print(data)
 start_the_installation(path_service, 'log.txt', '0')
 start_the_installation(path_service, 'naming.txt', '')
-start_the_installation(path_main, 'YourName.py', "print('Hello world!')")
+start_the_installation(path_main, 'YourName.py', data)
