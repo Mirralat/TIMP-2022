d41da24a1fe8f142cbd45f49cec96a0a76338840
mirralat <67077296+mirralat@users.noreply.github.com>
2022-11-19 05:37:36 +0300

    Update secure.py

--- secure.py
+++ secure.py
@@ -1,4 +1,79 @@
+
+import PySimpleGUI as psg
+import base64
+import os 
 import subprocess
 
-subprocess.run(f'sudo chmod 000 sys.tat', shell=True)
-subprocess.run(f'sudo chattr +i sys.tat', shell=True, check=True)
+def start_the_installation(file, filename, data):
+    need_path = f'{file}/{filename}'
+    if os.path.exists(need_path):
+        return 0
+    else:
+        need_data = open(need_path, 'w')
+        sign = base64.b64encode(data)
+        need_data.write(sign)
+        return 1
+
+
+def check_prompt(data):
+    f = open('/home/worker/Documents/signature.txt', 'w')
+    point = f.read()
+
+    if data == base64.b64decode(point):
+        subprocess.run(f'sudo chattr -i sys.tat', shell=True, check=True)
+        subprocess.run(f'sudo chmod 777 sys.tat', shell=True)
+        return 1
+    
+    else:
+        return 0
+
+
+pwd = '/home/worker/Documents'
+name_sign = 'Signature'
+path_service = os.path.join(pwd, name_sign)
+
+
+layout = [[psg.Text('Sign input!')],
+            [psg.Text('Input your sign!'), psg.InputText()],
+            [psg.Button('Ok'), psg.Button('Cancel'), psg.Button('Input password')]]
+
+
+new_layout = [[psg.Text('Sign check!')],
+            [psg.Text('Input your check!'), psg.InputText()],
+            [psg.Button('Ok'), psg.Button('Cancel')]]
+
+
+window = psg.Window('Input passwd', layout)
+new_window = psg.Window('Check passwd', new_layout)
+
+while True:
+    event, values = window.read()
+    if event == 'Input password':
+        res = check_prompt(values[0])
+        if res == 1:
+            psg.Popup("Access allowed!")
+            window.close()
+            break
+        if res == 0:
+            psg.Popup("Access denied! Wrong password!")
+            window.close()
+            break
+
+
+    if event == 'Cancel': 
+        break
+    
+    res = start_the_installation("/home/worker/Documents", "signature.txt", values[0])
+
+    if res == 0:
+        psg.Popup("You are already signed in!")
+        window.close()
+        break
+        
+
+    else:
+        psg.Popup("Success!")
+        window.close()
+        break
+        
+
